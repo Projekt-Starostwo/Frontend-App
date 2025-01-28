ce268c17b91f2e59df270169f5468e86bc76d6a2
Antek Ostrowski <antek.ostrowski@zs1mm.local>
2025-01-28 10:32:19 +0100

    ssr setup for school details

--- frontend-next-app/src/app/[id]/page.js
+++ frontend-next-app/src/app/[id]/page.js
@@ -1,5 +1,18 @@
 // strona poswicona szkole, najlepiej server component, chyba ze jakies guziki to wtedy mniejsze client componenty
 // po id szkoly pobrac o niej dane
-export default function Page({ params }) {
-  return <div>Hello, {params.id}!</div>
+
+import { getSchoolInfo } from "@/lib/queries";
+
+export default async function Page({ params }) {
+  const { id } = await params;
+  const mockId = "pzzb5d02ypz9rv19lb4mszys";
+  // const schoolInfo = await getSchoolInfo(id);
+  const schoolInfo = await getSchoolInfo(mockId);
+
+  return (
+    <div>
+      <h1>schoolInfo?.name</h1>
+      <h1>School ID: {schoolInfo?.id}</h1>
+    </div>
+  );
 }
--- frontend-next-app/src/lib/queries.js
+++ frontend-next-app/src/lib/queries.js
@@ -2,11 +2,36 @@
 export async function getDistrcts() {
   try {
     const res = await fetch(
-      'https://raw.githubusercontent.com/ppatrzyk/polska-geojson/master/powiaty/powiaty-medium.geojson'
-    )
-    const json = await res.json()
-    return json
+      "https://raw.githubusercontent.com/ppatrzyk/polska-geojson/master/powiaty/powiaty-medium.geojson"
+    );
+    const json = await res.json();
+    return json;
   } catch (error) {
-    throw new Error('Nie udało się pobrać danych o powiatach.')
+    throw new Error("Nie udało się pobrać danych o powiatach.");
+  }
+}
+const token = process.env.TOKEN;
+
+export async function getSchoolInfo(documentId) {
+  try {
+    const url = `http://localhost:1337/api/lista-szkols/${documentId}`;
+
+    const headers = {
+      method: "GET", // or "POST", "PUT", etc., depending on the API method
+      headers: {
+        Authorization: `Bearer ${token}`, // Include the Bearer token
+        "Content-Type": "application/json", // Set the content type to JSON (optional)
+      },
+    };
+
+    const res = await fetch(url, headers);
+
+    const jsonResponse = await res.json();
+
+    console.log(jsonResponse);
+    return jsonResponse;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 }
