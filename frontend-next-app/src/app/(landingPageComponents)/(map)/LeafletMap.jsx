0c048e65aee145007dddad108cd0bc585dab3714
Mateusz Pogoda <126457939+MatheWeather2137@users.noreply.github.com>
2025-01-30 12:23:13 +0100

    marker z cms

--- frontend-next-app/src/app/(landingPageComponents)/(map)/LeafletMap.jsx
+++ frontend-next-app/src/app/(landingPageComponents)/(map)/LeafletMap.jsx
@@ -1,42 +1,57 @@
-import dynamic from 'next/dynamic'
-import { MapContainer, TileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import Districts from './Districts'
-import CustomMarker from './CustomMarker'
-import { Button } from '@/components/ui/button'
-export const MAP_CENTER = [52.179, 21.57211]
+import dynamic from "next/dynamic";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Districts from "./Districts";
+import CustomMarker from "./CustomMarker";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState, useRef } from "react";
+
+export const MAP_CENTER = [52.179, 21.57211];
 
 const LeafletMap = ({ map, listOfSchools }) => {
+  console.log(listOfSchools);
+  const mapRef = useRef(null);
+
   return (
-    <div className='bg-gray-700 h-full w-2/3 p-10'>
-      <Button className='bg-black text-slate-300' onClick={() => map.map.setView(MAP_CENTER, 10)}>
+    <div className="bg-gray-700 h-full w-2/3 p-10">
+      <Button
+        className="bg-black text-slate-300"
+        onClick={() => {
+          if (mapRef.current) {
+            mapRef.current.setView(MAP_CENTER, 10);
+          } else {
+            console.warn("Mapa jeszcze nie jest gotowa!");
+          }
+        }}
+      >
         RESET MAP
       </Button>
+
       <MapContainer
         center={MAP_CENTER}
         zoom={10}
-        style={{ height: '100%', width: '100%' }}
+        style={{ height: "100%", width: "100%" }}
         maxZoom={17}
         minZoom={4}
-        maxBounds={[
-          [51.8, 20.6],
-          [52.5, 22.4113],
-        ]}
+        whenCreated={(mapInstance) => {
+          mapRef.current = mapInstance;
+        }}
         ref={map.setMap}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright*">OpenStreetMap</a> contributors*'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {listOfSchools.map((school) => {
-          return <CustomMarker key={school.id + 'marker'} school={school} />
+          console.log(school);
+          return <CustomMarker key={school.id} school={school} />;
         })}
 
         <Districts />
       </MapContainer>
     </div>
-  )
-}
+  );
+};
 
-export default dynamic(() => Promise.resolve(LeafletMap), { ssr: false })
+export default dynamic(() => Promise.resolve(LeafletMap), { ssr: false });
