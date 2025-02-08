fe8a86e6409b78334d5d84b1296dda56829e467a
antoni-ostrowski <antost360@gmail.com>
2025-02-08 11:19:43 +0100

    replace session storage with actual api call for school details

--- frontend-next-app/.next/static/chunks/src_app_[skrot_szkoly]_page_2b0f30.js
+++ frontend-next-app/.next/static/chunks/src_app_[skrot_szkoly]_page_2b0f30.js
@@ -5,8 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_[skrot_szkoly]_page_2b0f30.js",
   "chunks": [
-    "static/chunks/_62b171._.js",
-    "static/chunks/src_app_(landingPageComponents)_(map)_LeafletMap_jsx_a85583._.js"
+    "static/chunks/_28a96d._.js",
+    "static/chunks/src_app_(landingPageComponents)_(map)_LeafletMap_jsx_cdbdef._.js"
   ],
   "source": "dynamic"
 });
