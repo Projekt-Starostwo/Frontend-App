7436fa1a3863c91894eb79d33bac9bc71b96e7ec
antoni-ostrowski <antost360@gmail.com>
2025-02-09 18:20:00 +0100

    adapt new db schema in global search and query fn

--- frontend-next-app/src/app/(landingPageComponents)/(sidebarList)/ListOfSchools.jsx
+++ frontend-next-app/src/app/(landingPageComponents)/(sidebarList)/ListOfSchools.jsx
@@ -5,7 +5,7 @@ import SchoolListItem from './SchoolListItem'
 export default function ListOfSchools({ map, listOfSchools }) {
   return (
     <div className='h-full w-1/3 overflow-y-auto p-5 flex flex-col gap-10'>
-      {listOfSchools.data.map((school) => {
+      {listOfSchools?.data?.map((school) => {
         return <SchoolListItem map={map} key={school.id} school={school} />
       })}
     </div>
