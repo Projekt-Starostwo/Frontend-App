545c86848e39589311d23f13fe738acf154b7aee
Mateusz Pogoda <matimeusz123@gmail.com>
2025-03-08 23:09:05 +0100

    responsive navbar

--- frontend-next-app/src/components/Navbar.jsx
+++ frontend-next-app/src/components/Navbar.jsx
@@ -3,77 +3,103 @@
 import * as React from "react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 import { ThemeToggle } from "./ThemeToggle";
 import { ScrollProgress } from "./magicui/scroll-progress";
 import GlobalSearch from "./GlobalSearch";
+import { Menu, X } from "lucide-react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <div className="sticky top-0 z-50">
-      <div className="w-full flex justify-start items-center h-[7vh] border-b bg-background pl-4">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <div className="cursor-pointer">
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <h1 className="text-2xl font-bold">Nasze szkoły</h1>
-                  </NavigationMenuLink>
-                </div>
-              </Link>
-            </NavigationMenuItem>
-
-            <NavigationMenuItem>
-              <Link href="/kalkulator" legacyBehavior passHref>
-                <div className="cursor-pointer">
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <h1 className="text-2xl font-bold">Kalkulator</h1>
-                  </NavigationMenuLink>
-                </div>
-              </Link>
-            </NavigationMenuItem>
-
-            <div>
-              <Link href="/rekrutacja-vulcan" legacyBehavior passHref>
-                <div className="cursor-pointer">
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <h1 className="text-2xl font-bold">Rekrutacja Vulcan</h1>
-                  </NavigationMenuLink>
-                </div>
-              </Link>
-            </div>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <div className="absolute right-4 flex flex-row gap-4">
+      <div className="w-full flex justify-between items-center h-[7vh] border-b bg-background px-4 py-2">
+        <button
+          className="sm:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle Menu"
+        >
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+        <div className="hidden sm:flex items-center space-x-4">
+          <Link href="/" legacyBehavior passHref>
+            <a
+              className={`${navigationMenuTriggerStyle()} text-lg sm:text-xl font-bold`}
+            >
+              Nasze szkoły
+            </a>
+          </Link>
+          <Link href="/kalkulator" legacyBehavior passHref>
+            <a
+              className={`${navigationMenuTriggerStyle()} text-lg sm:text-xl font-bold`}
+            >
+              Kalkulator
+            </a>
+          </Link>
+          <Link href="/rekrutacja-vulcan" legacyBehavior passHref>
+            <a
+              className={`${navigationMenuTriggerStyle()} text-lg sm:text-xl font-bold`}
+            >
+              Rekrutacja Vulcan
+            </a>
+          </Link>
+        </div>
+        <div className="flex items-center gap-4">
           <GlobalSearch />
           <ThemeToggle />
         </div>
       </div>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } sm:hidden border-b bg-background px-4 py-2`}
+      >
+        <Link href="/" legacyBehavior passHref>
+          <a className="block py-2 text-lg sm:text-xl font-bold">
+            Nasze szkoły
+          </a>
+        </Link>
+        <Link href="/kalkulator" legacyBehavior passHref>
+          <a className="block py-2 text-lg sm:text-xl font-bold">Kalkulator</a>
+        </Link>
+        <Link href="/rekrutacja-vulcan" legacyBehavior passHref>
+          <a className="block py-2 text-lg sm:text-xl font-bold">
+            Rekrutacja Vulcan
+          </a>
+        </Link>
+      </div>
       <ScrollProgress className="" />
     </div>
   );
-  // return <div>fdjskl</div>;
 }
 
-const ListItem = ({ className, title, children, href, ...props }, ref) => {
-  return (
-    <li>
-      {/* <NavigationMenuLink asChild> */}
-      <Link
-        href={"/321"}
-        // ref={ref}
-        className={cn(
-          "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-          className
-        )}
-        {...props}
-      >
-        <div className="text-sm font-medium leading-none">{title}</div>
-        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-      </Link>
-      {/* </NavigationMenuLink> */}
-    </li>
-  );
-};
+const ListItem = React.forwardRef(
+  ({ className, title, children, href, ...props }, ref) => {
+    return (
+      <li>
+        <Link
+          href={href}
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </Link>
+      </li>
+    );
+  }
+);
 ListItem.displayName = "ListItem";
