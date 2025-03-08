579934a539a50f8ef4e443232da3940b384a0759
Mateusz Pogoda <matimeusz123@gmail.com>
2025-03-09 00:20:15 +0100

    responsive footer

--- frontend-next-app/src/app/(landingPageComponents)/Footer.jsx
+++ frontend-next-app/src/app/(landingPageComponents)/Footer.jsx
@@ -1,41 +1,43 @@
-import { Clock9, Mail, MapPinned, Phone } from 'lucide-react'
+import { Clock9, Mail, MapPinned, Phone } from "lucide-react";
 
-export default async function Footer() {
+export default function Footer() {
   return (
-    <div className='h-45 w-full border-t p-10 flex flex-col justify-center items-center gap-8'>
-      <h1 className='font-bold'>Starostwo Powiatowe w Mińsku Mazowieckim</h1>
+    <div className="w-full border-t p-10 flex flex-col items-center gap-8 justify-center">
+      <h1 className="font-bold text-center">
+        Starostwo Powiatowe w Mińsku Mazowieckim
+      </h1>
 
-      <div className='flex flex-row justify-center items-center gap-8'>
-        <div className='flex flex-row justify-center items-center gap-4'>
+      <div className="w-full flex flex-col md:flex-row flex-wrap justify-around items-start gap-8">
+        <div className="w-full md:w-auto flex flex-row items-center gap-4">
           <MapPinned size={40} />
-          <div className='flex flex-col'>
+          <div className="flex flex-col text-left">
             <p>ul. T.Kościuszki 3 05-300</p>
             <p>Mińsk Mazowiecki</p>
           </div>
         </div>
-        <div className='flex flex-row justify-start items-center gap-4'>
+        <div className="w-full md:w-auto flex flex-row items-center gap-4">
           <Phone size={40} />
-          <div className='flex flex-col justify-start items-start'>
+          <div className="flex flex-col text-left">
             <p>Tel. 25/ 759 87 00 </p>
             <p>Fax. 25/ 759 87 0</p>
           </div>
         </div>
-        <div className='flex flex-row justify-center items-center gap-4'>
+        <div className="w-full md:w-auto flex flex-row items-center gap-4">
           <Mail size={40} />
-          <p>boi@powiatminski.pl</p>
+          <p className="text-left">boi@powiatminski.pl</p>
         </div>
-        <div className='flex flex-row justify-center items-center gap-4'>
+        <div className="w-full md:w-auto flex flex-row items-center gap-4">
           <Clock9 size={40} />
-          <div>
-            <div className='w-52 flex flex-row justify-between items-center'>
+          <div className="flex flex-col text-left">
+            <div className="flex flex-row justify-between items-center">
               <p>Pon, Wt, Czw</p>
               <p>8:00-16:00</p>
             </div>
-            <div className='w-52 flex flex-row justify-between items-center'>
+            <div className="flex flex-row justify-between items-center">
               <p>Środa</p>
               <p>8:00-17:00</p>
             </div>
-            <div className='w-52 flex flex-row justify-between items-center'>
+            <div className="flex flex-row justify-between items-center">
               <p>Piątek</p>
               <p>8:00-15:00</p>
             </div>
@@ -43,5 +45,5 @@ export default async function Footer() {
         </div>
       </div>
     </div>
-  )
+  );
 }
