223e35bedda53135c3b41d7e2fe11ec2d83bd42e
Tryniu88 <127084934+Tryniu88@users.noreply.github.com>
2025-03-27 01:56:14 +0100

    zrobienie walidacji wszytskich lini autobusowych

--- frontend-next-app/src/app/(landingPageComponents)/(map)/BusLines.jsx
+++ frontend-next-app/src/app/(landingPageComponents)/(map)/BusLines.jsx
@@ -65,41 +65,56 @@ function BusLine({ busLine }) {
 // tu zmieniam tylko ta tablice w state w parent component
 export function BusSelection({ busLines, setBusLines }) {
   function handleCheckboxChange(e, line) {
-    console.log(e, line.name)
     setBusLines((prevBusLines) => {
-      const newBusLines = prevBusLines.map((busLine) => {
-        if (busLine.name === line.name) {
-          return {
-            ...busLine,
-            isActive: e,
-          }
+      const updatedBusLines = prevBusLines.map((busLine) => {
+        if (busLine.label === line.label) {
+          // Zaktualizuj stan aktualnej linii
+          return { ...busLine, isActive: e }
         }
         return busLine
       })
-      return newBusLines
+
+      // Po aktualizacji stanu, znajdź inną linię o tej samej nazwie (np. M1)
+      const otherLine = updatedBusLines.find(
+        (otherLine) => otherLine.label.startsWith(line.label.split(':')[0].trim()) && otherLine.label !== line.label
+      )
+
+      // Jeśli istnieje inna linia, ustaw jej atrybut disabled
+      if (otherLine) {
+        return updatedBusLines.map((busLine) => {
+          if (busLine.label === otherLine.label) {
+            return { ...busLine, disabled: e } // Ustaw disabled na wartość przeciwną do zaznaczenia
+          }
+          return busLine
+        })
+      }
+
+      return updatedBusLines
     })
   }
+
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button className='font-bold'>
-          <Bus /> Linie autobusowe
+        <Button className='font-bold bg-primary'>
+          <Bus className='mr-2 h-4 w-4' /> Linie autobusowe
         </Button>
       </PopoverTrigger>
-      <PopoverContent className='flex flex-col gap-2 w-fit'>
+      <PopoverContent className='flex flex-col gap-3 w-fit p-4'>
         {busLines.map((line) => {
           return (
-            <div key={line.name} className='flex items-center space-x-2'>
+            <div key={line.label} className='flex items-center space-x-2'>
               <Checkbox
                 checked={line.isActive}
-                id={line.name}
+                id={line.label}
                 onCheckedChange={(e) => {
                   handleCheckboxChange(e, line)
                 }}
+                disabled={line.disabled} // Użyj atrybutu disabled z obiektu linii
               >
-                {line.name}
+                {line.label}
               </Checkbox>
-              <Label htmlFor={line.name} className='cursor-pointer'>
+              <Label htmlFor={line.label} className='cursor-pointer'>
                 {line.label}
               </Label>
             </div>
